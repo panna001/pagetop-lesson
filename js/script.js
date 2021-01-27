836f524da8b3a2baeeb8c71fe62afc8a8d12dcda
石川大地 <ishikawadaichi@daichinoMacBook-Air.local>
2021-01-27 13:07:53 +0900

    [Finish]js-9

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-$('.theTarget').skippr({
-  transition : 'fade',
-  speed : 1000,
-  easing : 'easyOutQuart',
-  navType : 'block',
-  childrenElementType : 'div',
-  arrows : true,
-  autoPlay : true,
-  autoPlayDuration : 3000,
-  keyboardOnAlways : true,
-  hidePrevious : false
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
 });
